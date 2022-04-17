90a8d1735f9b66b85ef0b815c67a112d56ab16aa
jasonc-dev <jasoncohen07@gmail.com>
2022-04-17 13:36:51 +0100

    add api project to createJobs.groovy

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/jasonc-dev/theme-park-api.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
